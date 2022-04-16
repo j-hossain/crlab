6ae53e66f9b3cefbd09557a8bc26ee3067ffdd46
j-hossain <jahin.hsn101@gmail.com>
2022-04-17 00:02:43 +0600

    All people page fetched

--- scripts/allPeople.js
+++ scripts/allPeople.js
@@ -14,78 +14,32 @@ function getPeopleInfo(data){
 }
 
 function setAllPeople(peopleData){
-
+    console.log(peopleData);
+    let pepParent = document.getElementById("allPeopleDiv");
+    let pepTemplate = document.getElementById("personTemplate");
+    for(let i=peopleData.length-1;i>=0;i--){
+        let peopleBox = document.createElement('div');
+        peopleBox.innerHTML = pepTemplate.outerHTML;
+        peopleBox = peopleBox.firstChild;
+        peopleBox.classList.remove("disNone");
+        peopleBox.id="";
+        peopleBox = setPersonInfo(peopleData[i],peopleBox);
+        pepParent.append(peopleBox);
+    }
 }
 
-function ShowPerson(data){
-    document.getElementById("personName").innerHTML = data.acf.name;
-    document.getElementById("personDesignation").innerHTML = data.acf.designation;
-    document.getElementById("personResearchArea").innerHTML = data.acf.research_area;
-    document.getElementById("personLinkedin").href = data.acf.linkedin;
-    document.getElementById("personGoogleScholar").href = data.acf.scholar;
-    document.getElementById("personResearchGate").href = data.acf.research_gate;
-    getInfo("media/"+data.acf.profile_image,loadImage);
-}
-
-function loadImage(imageData){
-    document.getElementById("personImage").src = imageData.source_url;
-}
-
-function getPublicationList(pubs){
-    console.log(pubs);
-    let listPubs = new Array();
-    const urlParams = new URLSearchParams(window.location.search);
-    const personId = parseInt(urlParams.get("id"));
-    for(let i=0;i<pubs.length;i++){
-        for(j=1;j<=5;j++){
-            if(pubs[i].acf["author_"+j]==personId){
-                listPubs.push(pubs[i]);
-                break;
-            }
+function setPersonInfo(personData, div){
+    div.querySelector(".personName").innerHTML = personData.acf.name;
+    div.querySelector(".personDesignation").innerHTML = personData.acf.designation;
+    div.querySelector(".personResearchArea").innerHTML = personData.acf.research_area;
+    div.querySelector(".personLinkedin").href = personData.acf.linkedin;
+    div.querySelector(".personGoogleScholar").href = personData.acf.scholar;
+    div.querySelector(".personResearchGate").href = personData.acf.research_gate;
+    for(let i=0;i<mediaData.length;i++){
+        if(mediaData[i].id==personData.acf.profile_image){
+            div.querySelector(".personImage").src=mediaData[i].source_url;
         }
     }
-    showPublicationList(listPubs);
-}
-
-function showPublicationList(list){
-    console.log(list);
-    let pubParent = document.getElementById("listOfPublications");
-    let pubTemplate = document.getElementById("publicationTemplate");
-    for(let i=0;i<list.length;i++){
-        let pubBox = document.createElement('div');
-        pubBox.innerHTML = pubTemplate.outerHTML;
-        pubBox = pubBox.firstChild;
-        pubBox.classList.remove("disNone");
-        pubBox = setPublicationInfo(list[i],pubBox);
-        pubParent.append(pubBox);
-    }
-}
-
-function setPublicationInfo(data, div){
-    console.log(data);
-    let p = div.querySelector(".publicationInfo");
-    let a = div.querySelector(".PublicationLink");
-    a.href = "./publication.html?id="+data.id;
-    p.innerHTML = getAuthors(data) + " (" + data.acf.publish_year + ") " + data.acf.publication_tilte;
     return div;
 }
 
-function getAuthors(data){
-    let as = "";
-    let flag = false;
-    let x = data._embedded["acf:post"].length;
-    console.log(data);
-    for(let i=x-1;i>=0;i--){
-        let a = document.createElement('a');
-        a.href = "./people.html?id="+data._embedded["acf:post"][i].id;
-        a.innerHTML = data._embedded["acf:post"][i].acf.name;
-        if(flag){
-            as+=", ";
-        }
-        else{
-            flag=true;
-        }
-        as +=a.outerHTML;
-    }
-    return as;
-}
