1bb69c62e170df8081d144a7a6d9c38b2903a450
j-hossain <jahin.hsn101@gmail.com>
2022-04-18 16:48:15 +0600

    All people added in homepage

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,6 +1,7 @@
 getBanner();
 getAbout();
 getPublications();
+getAllPeople();
 
 function getBanner(){
     getInfo("banner",setBanner);
@@ -82,4 +83,53 @@ function getAuthors(data){
         as +=a.outerHTML;
     }
     return as;
+}
+
+
+let mediaData;
+
+
+function getAllPeople(){
+    getInfo("media",getPeopleInfo);
+}
+
+function getPeopleInfo(data){
+    console.log(data);
+    mediaData = data;
+    getInfo("people",setAllPeople);
+}
+
+function setAllPeople(peopleData){
+    console.log(peopleData);
+    let pepParent = document.getElementById("allPeopleDiv");
+    let pepTemplate = document.getElementById("personTemplate");
+    let min = peopleData.length;
+    if(min>3){
+        min = peopleData.length-3;
+    }
+    for(let i=peopleData.length-1;i>=min;i--){
+        let peopleBox = document.createElement('div');
+        peopleBox.innerHTML = pepTemplate.outerHTML;
+        peopleBox = peopleBox.firstChild;
+        peopleBox.classList.remove("disNone");
+        peopleBox.id="";
+        peopleBox = setPersonInfo(peopleData[i],peopleBox);
+        pepParent.append(peopleBox);
+    }
+}
+
+function setPersonInfo(personData, div){
+    div.querySelector(".personName").innerHTML = personData.acf.name;
+    div.querySelector(".personName").href = "./pages/people.html?id="+personData.id;
+    div.querySelector(".personDesignation").innerHTML = personData.acf.designation;
+    div.querySelector(".personResearchArea").innerHTML = personData.acf.research_area;
+    div.querySelector(".personLinkedin").href = personData.acf.linkedin;
+    div.querySelector(".personGoogleScholar").href = personData.acf.scholar;
+    div.querySelector(".personResearchGate").href = personData.acf.research_gate;
+    for(let i=0;i<mediaData.length;i++){
+        if(mediaData[i].id==personData.acf.profile_image){
+            div.querySelector(".personImage").src=mediaData[i].source_url;
+        }
+    }
+    return div;
 }
