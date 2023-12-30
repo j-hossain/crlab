f9efa304eefbeba267c7fc52e7819d8c177325e7
j-hossain <jahin.hsn101@gmail.com>
2023-12-30 22:52:05 +0600

    Added ordering for people and changed people tag

--- scripts/allPeople.js
+++ scripts/allPeople.js
@@ -1,38 +1,44 @@
 getAllPeople();
 
 
-function getAllPeople(){
+function getAllPeople() {
     getPeopleInfo();
 }
 
-function getPeopleInfo(){
-    getInfo("people",setAllPeople);
+function getPeopleInfo() {
+    getInfo("people", setAllPeople);
 }
 
-function setAllPeople(peopleData){
-    console.log(peopleData);
+function setAllPeople(peopleData) {
+    peopleData.sort((a, b) => {
+        if (a.acf.order != b.acf.order) {
+            return a.acf.order - b.acf.order;
+        }
+        if (a.date < b.date) return -1;
+        return 1;
+    })
     let pepParent = document.getElementById("allPeopleDiv");
     let pepTemplate = document.getElementById("personTemplate");
-    for(let i=peopleData.length-1;i>=0;i--){
+    for (let i = 0; i < peopleData.length; i++) {
         let peopleBox = document.createElement('div');
         peopleBox.innerHTML = pepTemplate.outerHTML;
         peopleBox = peopleBox.firstChild;
         peopleBox.classList.remove("disNone");
-        peopleBox.id="";
-        peopleBox = setPersonInfo(peopleData[i],peopleBox);
+        peopleBox.id = "";
+        peopleBox = setPersonInfo(peopleData[i], peopleBox);
         pepParent.append(peopleBox);
     }
 }
 
-function setPersonInfo(personData, div){
+function setPersonInfo(personData, div) {
     div.querySelector(".personName").innerHTML = personData.acf.name;
-    div.querySelector(".personLink").href = "./people.html?id="+personData.id;
+    div.querySelector(".personLink").href = "./people.html?id=" + personData.id;
     div.querySelector(".personDesignation").innerHTML = personData.acf.designation;
     div.querySelector(".personResearchArea").innerHTML = personData.acf.research_area;
     div.querySelector(".personLinkedin").href = personData.acf.linkedin;
     div.querySelector(".personGoogleScholar").href = personData.acf.scholar;
     div.querySelector(".personResearchGate").href = personData.acf.research_gate;
-    setMedia(personData.acf.profile_image,div.querySelector(".personImage"))
+    setMedia(personData.acf.profile_image, div.querySelector(".personImage"))
     return div;
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
-function getInfo(gotothis, dothis){
+function getInfo(gotothis, dothis) {
     // for development purpose
     // const baseurl = "http://localhost/crlabdev";
     const baseurl = "https://crlab.jahin.xyz/index.php";
     const requrl = baseurl + "/wp-json/wp/v2/" + gotothis;
     const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
+    xhttp.onload = function () {
         dothis(JSON.parse(this.responseText));
     }
     xhttp.open("GET", requrl);
     xhttp.send();
 }
 
-function setMedia(id, element){
+function setMedia(id, element) {
     element.src = "../img/loading.gif";
-    getInfo("media/"+id,(mediaData)=>{
+    getInfo("media/" + id, (mediaData) => {
         element.src = mediaData.source_url;
     });
 }
@@ -21,15 +21,15 @@ function setMedia(id, element){
 getContactInfo();
 
 //getting the contact info for footer section
-function getContactInfo(){
-    getInfo("contact_information",(contactData)=>{
-        if(contactData.length==0) return
+function getContactInfo() {
+    getInfo("contact_information", (contactData) => {
+        if (contactData.length == 0) return
         let footer = document.getElementById("footer");
         footer.querySelector(".address").innerHTML = contactData[0].acf.address;
         footer.querySelector(".emailAddress").innerHTML = contactData[0].acf.email_address;
-        footer.querySelector(".emailAddress").href = "mailto:"+contactData[0].acf.email_address;
-        footer.querySelector(".phone").innerHTML ="Phone : " + contactData[0].acf.mobile_number;
-        footer.querySelector(".emailLink").href = "mailto:"+contactData[0].acf.email_address;
+        footer.querySelector(".emailAddress").href = "mailto:" + contactData[0].acf.email_address;
+        footer.querySelector(".phone").innerHTML = "Phone : " + contactData[0].acf.mobile_number;
+        footer.querySelector(".emailLink").href = "mailto:" + contactData[0].acf.email_address;
         footer.querySelector(".facebookLink").href = contactData[0].acf.facebook_link;
         footer.querySelector(".twitterLink").href = contactData[0].acf.twitter_link;
         footer.querySelector(".googlePlusLink").href = contactData[0].acf.google_link;
