7e38de8ac9c640a0b7655727d7ee89c5cfa6fe2f
j-hossain <jahin.hsn101@gmail.com>
2024-01-10 18:00:14 +0600

    Map fixed and more nullcheck added

--- scripts/allBlogs.js
+++ scripts/allBlogs.js
@@ -1,75 +1,74 @@
 initFunctions();
 
-function initFunctions(){
+function initFunctions() {
     getAllBlogs();
 }
 
-function getAuthors(data){
+function getAuthors(data) {
     let as = "";
     let flag = false;
-    let x = data._embedded["acf:post"].length;
-    for(let i=x-1;i>=0;i--){
+    let x = data._embedded["acf:post"]?.length;
+    for (let i = x - 1; i >= 0; i--) {
         let a = document.createElement('a');
-        a.href = "./people.html?id="+data._embedded["acf:post"][i].id;
-        a.innerHTML = data._embedded["acf:post"][i].acf.name;
-        if(flag){
-            as+=", ";
+        a.href = "./people.html?id=" + data?._embedded["acf:post"][i]?.id;
+        a.innerHTML = data?._embedded["acf:post"][i]?.acf?.name;
+        if (flag) {
+            as += ", ";
         }
-        else{
-            flag=true;
+        else {
+            flag = true;
         }
-        as +=a.outerHTML;
+        as += a.outerHTML;
     }
     return as;
 }
 
 //working on getting some recent blogs
-function getAllBlogs(){
-    getInfo("blog?_embed",setAllBlogs);
+function getAllBlogs() {
+    getInfo("blog?_embed", setAllBlogs);
 }
 
-function setAllBlogs(blogData){
+function setAllBlogs(blogData) {
     let blogParent = document.getElementById("allBlogDiv");
     let blogTemplate = document.getElementById("blogTemplate");
-    
-    for(let i=blogData.length-1;i>=0;i--){
+
+    for (let i = blogData.length - 1; i >= 0; i--) {
         let blogBox = document.createElement('div');
         blogBox.innerHTML = blogTemplate.outerHTML;
         blogBox = blogBox.firstChild;
         blogBox.classList.remove("disNone");
-        blogBox.id="";
-        blogBox = setBlogInfo(blogData[i],blogBox);
+        blogBox.id = "";
+        blogBox = setBlogInfo(blogData[i], blogBox);
         blogParent.append(blogBox);
     }
 }
 
-function setBlogInfo(blogData, div){
-    setMedia(blogData.featured_media,div.querySelector(".blogImage"));
+function setBlogInfo(blogData, div) {
+    setMedia(blogData.featured_media, div.querySelector(".blogImage"));
     div.querySelector(".blogWriter").innerHTML = getAuthors(blogData);
     div.querySelector(".blogCategories").innerHTML = getblogCategories(blogData);
-    div.querySelector(".blogTitle").innerHTML = blogData.title.rendered;
-    div.querySelector(".blogTitle").href = "./blog.html?id=" + blogData.id;
-    div.querySelector(".blogLink").href = "./blog.html?id=" + blogData.id;
-    div.querySelector(".blogExerpt").innerHTML = blogData.excerpt.rendered;
-    div.querySelector(".publishDate").innerHTML = new Date(blogData.date.toString()).toUTCString();
+    div.querySelector(".blogTitle").innerHTML = blogData?.title?.rendered;
+    div.querySelector(".blogTitle").href = "./blog.html?id=" + blogData?.id;
+    div.querySelector(".blogLink").href = "./blog.html?id=" + blogData?.id;
+    div.querySelector(".blogExerpt").innerHTML = blogData?.excerpt?.rendered;
+    div.querySelector(".publishDate").innerHTML = new Date(blogData?.date.toString()).toUTCString();
     return div;
 }
 
-function getblogCategories(data){
+function getblogCategories(data) {
     let cats = "";
     let flag = false;
     // console.log(data._embedded["wp:term"]);
-    let catos = data._embedded["wp:term"][0];
+    let catos = data?._embedded["wp:term"][0];
     let x = catos.length;
-    console.log(catos);
-    for(let i=x-1;i>=0;i--){
-        if(flag){
-            cats+=", ";
+    for (let i = x - 1; i >= 0; i--) {
+        if (flag) {
+            cats += ", ";
         }
-        else{
-            flag=true;
+        else {
+            flag = true;
         }
-        cats +=catos[i].name;
+        cats += catos[i]?.name;
     }
     return cats;
 }
