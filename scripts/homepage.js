ca044355552c54ad91a04ff3a4000d140b9f1223
j-hossain <jahin.hsn101@gmail.com>
2024-01-15 21:05:32 +0600

    All people changed in homepage

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -121,10 +121,10 @@ function getAuthors(data) {
 }
 
 function getAllPeople() {
-    getInfo("people", setAllPeople);
+    getInfo("people?_embed", mapPeopleType);
 }
 
-function setAllPeople(peopleData) {
+function mapPeopleType(peopleData) {
     peopleData.sort((a, b) => {
         if (a.acf.order != b.acf.order) {
             return a.acf.order - b.acf.order;
@@ -132,10 +132,35 @@ function setAllPeople(peopleData) {
         if (a.date < b.date) return -1;
         return 1;
     })
-    let pepParent = document.getElementById("allPeopleDiv");
-    let pepTemplate = document.getElementById("personTemplate");
-    let mini = Math.min(peopleData.length, 4);
-    for (let i = 0; i < mini; i++) {
+    let mapedPeopleData = {}
+    for (i in peopleData) {
+        let memberType = peopleData[i]?.acf?.member_type
+        if (mapedPeopleData[memberType] == undefined) {
+            mapedPeopleData[memberType] = new Array()
+        }
+        mapedPeopleData[memberType].push(peopleData[i])
+    }
+
+    for (memberType in mapedPeopleData) {
+        setAllPeople(mapedPeopleData[memberType], getMemberTypeParentDiv(memberType))
+    }
+}
+
+function getMemberTypeParentDiv(memberType) {
+    let mainDiv = document.getElementById('business')
+    let memberDivTemplate = mainDiv.querySelector('.container')
+    let memberDiv = document.createElement('div')
+    memberDiv.innerHTML = memberDivTemplate.outerHTML
+    memberDiv = memberDiv.firstChild
+    memberDiv.classList.remove('disNone')
+    mainDiv.append(memberDiv)
+    memberDiv.querySelector('.business_1').getElementsByTagName('h2')[0].innerHTML = memberType
+    return memberDiv.querySelector(".allPeopleDiv")
+}
+
+function setAllPeople(peopleData, pepParent) {
+    let pepTemplate = pepParent.querySelector(".personTemplate");
+    for (let i = 0; i < peopleData.length; i++) {
         let peopleBox = document.createElement('div');
         peopleBox.innerHTML = pepTemplate.outerHTML;
         peopleBox = peopleBox.firstChild;
@@ -148,13 +173,13 @@ function setAllPeople(peopleData) {
 
 function setPersonInfo(personData, div) {
     div.querySelector(".personName").innerHTML = personData.acf.name;
-    div.querySelector(".personLink").href = "./pages/people.html?id=" + personData.id;
+    div.querySelector(".personLink").href = "./people.html?id=" + personData.id;
     div.querySelector(".personDesignation").innerHTML = personData.acf.designation;
     div.querySelector(".personResearchArea").innerHTML = personData.acf.research_area;
     div.querySelector(".personLinkedin").href = personData.acf.linkedin;
     div.querySelector(".personGoogleScholar").href = personData.acf.scholar;
     div.querySelector(".personResearchGate").href = personData.acf.research_gate;
-    setMedia(personData.acf.profile_image, div.querySelector(".personImage"));
+    setMedia(personData.acf.profile_image, div.querySelector(".personImage"))
     return div;
 }
 
